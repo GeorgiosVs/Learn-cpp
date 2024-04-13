f0d2fed578443b5c35602653d001ccccd85fdc8b
GeorgiosVs <158276611+GeorgiosVs@users.noreply.github.com>
2024-04-13 13:24:35 +0300

    Add files via upload
    Συνάρτηση επιστροφής τιμής mul()

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int mul(int x, int y);      // <---- mul prototype
+
+int main() {
+    
+    int answer;
+    
+    answer = mul(10, 11);   //Δήλωση αριθμών της επιστρεφόμενης τιμής x και y.
+    cout << "The answer is " << answer << endl;
+    
+    return 0;
+}
+
+
+
+
+//Αυτή η συνάρτηση επιστρέφει μία τιμή.
+int mul(int x, int y){      //Κάλεσμα της mul
+    return x * y;           //επιστροφή του γινομένου των x και y
+}
