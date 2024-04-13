6073ddd84edfad1d2f1ecfc114863913818ba1be
GeorgiosVs <158276611+GeorgiosVs@users.noreply.github.com>
2024-04-14 00:12:49 +0300

    #cpp
    Μικρό πρόγραμμα εκτυπώνει την Αγγλική αλφάβητο. (Άσκηση κατανόησης)

---
+++ alphabet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    char letter;    //Δήλωση μεταβλητής.
+    
+    
+    for (letter = 'A'; letter<='Z'; letter++) {
+        cout << letter << endl;
+    }
+    
+    return 0;
+}
+//Πρόγραμμα σε γλώσσα Cpp εκτυπώνει την Αγγλική αλφάβητο.
