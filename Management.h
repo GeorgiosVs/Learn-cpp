206730c6a6b351b75a3efa6e2a69cf384dc343c4
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-05-09 21:27:20 +0300

    info project cpp

--- Management.h
+++ Management.h
@@ -1,9 +1,14 @@
+/*
+ This is the Management.h file that contains all the
+ functions prototypes from logo.cpp file
+ */
 #ifndef Management_h
 #define Management_h
 class Management
 {
 public:
-        void logo();
+    void logo();                        //Calling logo() from logo.cpp file
+    std::string name();                 //Calling name() from logo.cpp file
 };
 
 #endif /* Management_h */
