65ec074f187f6a298f55ef9e2085de236cee1b06
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-05-11 12:48:55 +0300

    class

--- Management.h
+++ Management.h
@@ -1,14 +1,62 @@
 /*
- This is the Management.h file that contains all the
- functions prototypes from logo.cpp file
- */
+Αυτό είναι το αρχείο Management.h που περιέχει όλες τις
+προτότυπες συναρτήσεις από το αρχείο logo.cpp είναι η κλάση για το αρχείο main.cpp
+*/
 #ifndef Management_h
 #define Management_h
-class Management
-{
+
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<sstream>
+
+//Δήλωση δεδομένων User & Ticket
+struct User {
+    std::string username;
+    std::string password;
+    std::string email;
+    std::string phone;
+};
+
+struct Ticket {
+    std::string ticketNumber;
+    std::string username;
+    std::string destination;
+    int cost;
+    bool roundTrip;
+    int luggageWeight;
+};
+
+class Management {
+private:
+    
+    //Λίστα για τα εισιτήρια
+    std::vector<Ticket> tickets;
+    
+    //Χρήστης που έχει συνδεθεί
+    User loggedInUser;
+
+    //Συναρτήσεις εισιτηρίων private
+    void printReceipt(const Ticket& ticket);
+    void purchaseTicket();
+    void viewTicketInfo(const std::string& ticketNumber);
+    bool usernameExists(const std::string& username);
+    
 public:
-    void logo();                        //Calling logo() from logo.cpp file
-    std::string name();                 //Calling name() from logo.cpp file
+    
+    //Συναρτήσεις public από το αρχείο logo.cpp
+    
+    //Συνάρτηση εμφάνισης λογότυπου
+    void logo();
+    
+    //Συνάρτηση εγγραφής νέου χρήστη
+    void signUp();
+    
+    //Συνάρτηση εισόδου χρήστη
+    void login();
+    
+    //Συνάρτηση για αυτό που θα επιλέξει ο χρήστης
+    int userChoice();
 };
 
 #endif /* Management_h */
