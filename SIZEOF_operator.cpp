80d1fbc9d95850ed01435c13be69e2a5e01c20ca
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-05-13 22:09:55 +0300

    sizeof

---
+++ SIZEOF_operator.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){	
+	
+	char c;
+	float b;
+	int a;
+	cout<<"Oi akeraioi arithmoi exoun megethos :"<< sizeof(a)<<endl;
+	
+	cout<<"Oi pragmatikoi arithmoi exoun megethos :" << sizeof(b)<<endl;
+	
+	cout<<"Oi xaraktires exoun megethos :" << sizeof(c)<<endl;
+	
+	cout<<"Parakalw dwste enan akeraio arithmo :",a;
+	cin>>a;
+	
+	cout<<"\nParakalw dwste enan pragmatiko arithmo :",b;
+	cin>>b;
+	
+	cout<<"\nDwse enan xaraktira :",c;
+	cin>>c;
+	
+	cout<<"\nO akeraios arithmos poy dwsate einai :" << a;
+	
+	cout<<"\nO pragmatikos arithmos poy dwsate einai :" << b;
+	
+	cout<<"\nO xaraktiras poy dwsate einai :" << c<<endl;
+	
+	system("\npause");
+}
+
+/*Askhsh sizeof(c) operator, emfanizei posh xwrithkothta katalamvanoyn oi metavlhtes
+C++*/
