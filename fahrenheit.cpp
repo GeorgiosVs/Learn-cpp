b88254612c7b408a61c88809b8e9643232db7fd0
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-05-15 21:31:38 +0300

    Fahrenheit cpp

---
+++ fahrenheit.cpp
@@ -0,0 +1,20 @@
+//Metatroph apo Fahrenheit se Celsius
+#include <iostream>
+using namespace std;
+int main(){
+	
+	//Metavlhtes
+	float a;
+	cout << "Metatroph apo fahrenheit se Celsius" << endl;
+	
+	cout << "Dwste va8mo se fahrenheit: ";
+	cin>>a;
+	
+	a=(a-32)*5/9;
+	
+	cout << "Oi va8moi apo fahrenheit se Celsius einai: " << a << endl;
+	
+	system("pause");
+}
+/*Typos metatropis Fahrenheit (-32*5/9), a=(a-32)*5/9;
+C++*/
