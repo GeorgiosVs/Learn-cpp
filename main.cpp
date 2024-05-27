dd63a2d54a0a64a5f2c32e7b799147c2d9b5a282
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2024-05-27 10:24:24 +0300

    function that calculates the length

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-/*
-Πρόγραμμα σε C++
-Terminal Αεροδρομίου / Δημιουργία νέου χρήστη / Σύνδεση χρήστη / Επιλογή προορισμού
-Επιλογή μετ' επιστροφής ή όχι / Επιλογή αποσκευών / Πληροφορίες εισητηρίου
-*/
-#include "Management.h"
+#include<iostream>
+using namespace std;
+
+//function declaration
+int stringLength(char *);
 
 int main() {
-    Management manager;         //Δημιουργία αντικειμένου της κλάσης Management
-        manager.logo();         //Εμφάνιση Λογότυπου
-        manager.userChoice();   //Μενού επιλογών για τον χρήστη
-
+    
+    char text[100];
+    int length;
+    
+    cout << "\nEnter text (max 100 characters) :";
+    cin>>text;
+    
+    length = stringLength(text);
+    
+    cout << "Input text is :" << text << endl;
+    cout << "Length is :" << length << endl;
     return 0;
+}
+
+int stringLength(char *str){
+    int len=0;
+    
+    //calculating string length
+    for (len=0; str[len]!='\0'; len++);
+    
+    return len;
 }
